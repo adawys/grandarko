b43ada671ec6b4ffe83bcab5f3af8ea19fcac2a3
System Administrator <root@MacBook-Pro-Adam.local>
2020-06-10 14:14:54 +0200

    rwd v0.1

--- js/nav.js
+++ js/nav.js
@@ -1,12 +1,24 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
-  });
+window.onload = function() {
+  const first = document.getElementById('first');
+  const list = document.getElementById('right-show');
+  const close1 = document.getElementById('close1');
+  const close2 = document.getElementById('close2');
+  const close3 = document.getElementById('close3');
 
-  // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
-  // var collapsibleElem = document.querySelector('.collapsible');
-  // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
-
-  // Or with jQuery
-
-  
+  function viewMenu() {
+      first.addEventListener('click', viewList);
+  }
+  function closeMenu() {
+      close1.addEventListener("click", hideList);
+      close2.addEventListener("click", hideList);
+      close3.addEventListener("click", hideList);
+  }
+  function viewList(){
+      list.classList.toggle('list-show');
+  }
+  function hideList(){
+      list.classList.remove("list-show")
+  }
+  viewMenu();
+  closeMenu();
+};
